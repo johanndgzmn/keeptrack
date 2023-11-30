902370206bd62fa6598fadedc4a205d1b13265ff
johanndgzmn <johanndgzmn@gmail.com>
2023-11-30 10:47:03 +0800

    Updated App.js and added projectspage.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <blockquote cite="Benjamin Franklin">
+        Tell me and I forget, teach me and I may remember, involve me and I learn.
+    </blockquote>
+  );
+}
+
+export default App;
